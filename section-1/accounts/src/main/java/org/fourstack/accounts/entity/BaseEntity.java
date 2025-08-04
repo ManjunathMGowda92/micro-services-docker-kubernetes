e56a1e8a13ea6e6a52c3d0c0df7e2f81193ab00d
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-08-04 21:13:15 +0530

    Code to add the creation user and update user details.

--- section-1/accounts/src/main/java/org/fourstack/accounts/entity/BaseEntity.java
+++ section-1/accounts/src/main/java/org/fourstack/accounts/entity/BaseEntity.java
@@ -1,28 +1,38 @@
 package org.fourstack.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.fourstack.accounts.audit.AuditAwareImpl;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditAwareImpl.class)
 @Getter
 @Setter
 @ToString
-public class BaseEntity {
-
-    @Column(updatable = false)
-    private LocalDateTime creationTimestamp;
-
-    @Column(updatable = false)
+public abstract class BaseEntity {
+    @Column(updatable = false, nullable = false)
+    @CreatedBy
     private String createdBy;
 
-    @Column(insertable = false)
-    private LocalDateTime updatedTimestamp;
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime creationTimestamp;
 
     @Column(insertable = false)
+    @LastModifiedBy
     private String updatedBy;
+
+    @Column(insertable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedTimestamp;
 }
