a8eaf13a614810efd8dc4a49155fb2b19b7bb5df
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-08-04 22:24:25 +0530

    Added Open API 3.0 Swagger documentation.

--- section-1/accounts/src/main/java/org/fourstack/accounts/dto/AccountsDto.java
+++ section-1/accounts/src/main/java/org/fourstack/accounts/dto/AccountsDto.java
@@ -1,5 +1,6 @@
 package org.fourstack.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,12 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(name = "Accounts", description = "Schema to hold Accounts information")
 public class AccountsDto {
+    @Schema(description = "Account number value")
     private Long accountNumber;
 
+    @Schema(description = "Account type", examples = {"Savings", "Current"})
     @NotEmpty(message = "Account Type cannot be null or empty")
     private String accountType;
 
+    @Schema(description = "Branch address details", example = "#121, 4th Street, 11th Main, Bellanduru - 560102")
     @NotEmpty(message = "Branch Address cannot be null or empty")
     private String branchAddress;
 }
