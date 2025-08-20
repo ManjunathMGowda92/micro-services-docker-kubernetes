641b4f2cebe7f0c3937300f1fe8746cfff33395e
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-08-20 19:48:09 +0530

    Configuring the properties using @Value annotation.

---
+++ section-3/v1/accounts/src/main/java/org/fourstack/accounts/controller/AppInfoController.java
@@ -0,0 +1,47 @@
+package org.fourstack.accounts.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts/info")
+@Tag(
+        name = "REST API for fetching Application information"
+)
+public class AppInfoController {
+
+    @Value("${app.details.name}")
+    private String applicationName;
+
+    @Value("${app.details.author}")
+    private String author;
+
+    @Value("${app.details.description}")
+    private String description;
+
+    @Value("${app.details.version}")
+    private String version;
+
+    @Operation(
+            summary = "API to get the application information",
+            description = "REST API to fetch the application information"
+    )
+    @GetMapping
+    public ResponseEntity<String> getApplicationInfo() {
+        String appInfo = """
+                {
+                    "applicationName": "%s",
+                    "author": "%s",
+                    "description" : "%s",
+                    "version" : "%s"
+                }
+                """.formatted(applicationName, author, description, version);
+
+        return ResponseEntity.ok(appInfo);
+    }
+}
