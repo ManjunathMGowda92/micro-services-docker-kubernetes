77906627f6019c2d18b31617720420e416e94d3d
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-08-20 22:04:06 +0530

    Configuring the properties using @ConfigurationProperties

---
+++ section-3/v1/loans/src/main/java/org/fourstack/loans/config/AppInfoConfig.java
@@ -0,0 +1,16 @@
+package org.fourstack.loans.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "app.details")
+@Getter
+@Setter
+public class AppInfoConfig {
+    private String name;
+    private String author;
+    private String description;
+    private String version;
+}
