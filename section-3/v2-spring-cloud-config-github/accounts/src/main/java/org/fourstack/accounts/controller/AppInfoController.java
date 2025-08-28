c011a2ebce08b0e7c99c26bb093be1339cde04a9
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-08-28 19:57:14 +0530

    Added changes to support the actuator endpoint for refreshing the configurations at runtime.

--- section-3/v2-spring-cloud-config-github/accounts/src/main/java/org/fourstack/accounts/controller/AppInfoController.java
+++ section-3/v2-spring-cloud-config-github/accounts/src/main/java/org/fourstack/accounts/controller/AppInfoController.java
@@ -3,6 +3,7 @@ package org.fourstack.accounts.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/accounts/info")
+@RefreshScope
 @Tag(
         name = "REST API for fetching Application information"
 )
