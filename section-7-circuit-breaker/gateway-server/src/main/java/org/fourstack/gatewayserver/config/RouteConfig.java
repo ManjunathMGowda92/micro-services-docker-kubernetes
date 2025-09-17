4449155a805a94f2454d84fc5db3d54fe3cf413b
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-09-17 07:20:02 +0530

    Circuit Breaker configurations at Gateway server.

--- section-7-circuit-breaker/gateway-server/src/main/java/org/fourstack/gatewayserver/config/RouteConfig.java
+++ section-7-circuit-breaker/gateway-server/src/main/java/org/fourstack/gatewayserver/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package org.fourstack.gatewayserver.config;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,12 +17,18 @@ public class RouteConfig {
                 .routes()
                 .route(apiPath -> apiPath
                         .path("/bank-app/accounts/**")
-                        .filters(api -> api.rewritePath("/bank-app/accounts/(?<segment>.*)", "/accounts-service/api/v1/accounts/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(api -> api.rewritePath("/bank-app/accounts/info", "/accounts-service/accounts/info")
+                                .rewritePath("/bank-app/accounts/(?<segment>.*)", "/accounts-service/api/v1/accounts/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNTS"))
                 .route(apiPath -> apiPath
                         .path("/bank-app/customers/**")
-                        .filters(api -> api.rewritePath("/bank-app/customers/(?<segment>.*)", "/accounts-service/api/v1/customers/${segment}"))
+                        .filters(api -> api.rewritePath("/bank-app/customers/(?<segment>.*)", "/accounts-service/api/v1/customers/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("customerCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNTS"))
                 .route(apiPath -> apiPath
                         .path("/bank-app/loans/**")
