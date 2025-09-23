9530c3008d1461af2db346aaf4de9ff00bf020e1
ManjunathMGowda92 <manjunathcs052@gmail.com>
2025-09-23 19:49:06 +0530

    Base code for rate limiter.

---
+++ section-8-rate-limiter/accounts/src/main/java/org/fourstack/accounts/controller/CustomerController.java
@@ -0,0 +1,122 @@
+package org.fourstack.accounts.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.fourstack.accounts.dto.CompleteCustomerDetailsDto;
+import org.fourstack.accounts.dto.CustomerAccountRequestDto;
+import org.fourstack.accounts.dto.CustomerDetailsDto;
+import org.fourstack.accounts.dto.CustomerDto;
+import org.fourstack.accounts.dto.ErrorResponseDto;
+import org.fourstack.accounts.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor(onConstructor_ = @Lazy)
+@Validated
+@Tag(
+        name = "REST-API for Customer Information",
+        description = "CRUD APIs to support FETCH Customer details details."
+)
+public class CustomerController {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    private final CustomerService customerService;
+
+    @Operation(
+            summary = "API to fetch complete customer information (customer, accounts, loans and cards info) based on mobile number.",
+            description = "REST API to fetch complete customer information (customer, accounts, loans and cards info) based on mobile number.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer details"),
+                    @ApiResponse(responseCode = "404", description = "No Customer details found for given mobile number",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
+    @GetMapping("/details/{mobileNumber}")
+    public ResponseEntity<CompleteCustomerDetailsDto> retrieveCompleteDetailsByMobileNumber(
+            @PathVariable @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            String mobileNumber) {
+        return customerService.retrieveCompleteCustomerInfo(mobileNumber);
+    }
+
+
+    @Operation(
+            summary = "API to fetch customer information based on mobile number.",
+            description = "REST API to fetch customer information based on mobile number.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer details"),
+                    @ApiResponse(responseCode = "404", description = "No Customer details found for given mobile number",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
+    @Retry(name = "retrieveDetailsByMobileNumber", fallbackMethod = "retrieveDetailsByMobileNumberFallback")
+    @GetMapping("/fetch-by-mobile/{mobileNumber}")
+    public ResponseEntity<CustomerDto> retrieveDetailsByMobileNumber(
+            @PathVariable @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            String mobileNumber) {
+        logger.info("Retrieve Customer details by mobile number : {}", mobileNumber);
+        return customerService.retrieveCustomerByMobileNumber(mobileNumber);
+    }
+
+    public ResponseEntity<CustomerDto> retrieveDetailsByMobileNumberFallback(String mobileNumber, Throwable throwable) {
+        logger.error("Executing Fallback mechanism for retrieveDetailsByMobileNumber:{} due to exception",
+                mobileNumber, throwable);
+        CustomerDto dummyResponse = CustomerDto.builder().mobileNumber(mobileNumber)
+                .email("dummy@gmail.com")
+                .name("Dummy Response")
+                .build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(dummyResponse);
+    }
+
+    @Operation(
+            summary = "API to fetch customer information based on Email ID.",
+            description = "REST API to fetch customer information based on Email ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer details"),
+                    @ApiResponse(responseCode = "404", description = "No Customer details found for given Email ID",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
+    @GetMapping("/fetch-by-email/{email}")
+    public ResponseEntity<CustomerDto> retrieveDetailsByEmail(
+            @PathVariable @Email(message = "Invalid email value received") String email) {
+        return customerService.retrieveCustomerByEmail(email);
+    }
+
+    @Operation(
+            summary = "API to fetch complete customer information along with account details based on mobile number or Email ID.",
+            description = "REST API fetch complete customer information along with account details based on mobile number or Email ID. " +
+                    "User can use either mobile number or Email ID to fetch the details. If both are provided, then priority " +
+                    "will be provided for mobile number.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer details"),
+                    @ApiResponse(responseCode = "404", description = "No Customer details found for given Email ID or mobile number",
+                            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+            }
+    )
+    @PostMapping("/retrieve-customer")
+    public ResponseEntity<CustomerDetailsDto> retrieveCompleteCustomerDetailsByMobile(
+            @RequestBody @Valid CustomerAccountRequestDto dto) {
+        return customerService.retrieveCompleteCustomerDetails(dto);
+    }
+
+}
